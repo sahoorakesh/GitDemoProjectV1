e8fb1d4a09f66d21c4e4324fa98a5cc68ae31cdc
sahoo <sahoo@RS-LAPTOP>
2021-03-03 17:48:07 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungary="yes";
+		if(hungary=="yes") {
+			System.out.println("eat something----------");
+		}
+		else
+		{
+			System.out.println("Do Work-------");
+		}
+	}
+
+}
